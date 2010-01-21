d52872e3812e2cdf625203471bf95234244c27a4
Jeff Tucker <trydionel@gmail.com>
2010-01-21 18:52:23 -0500

    Working on it

--- git-pick.rb
+++ git-pick.rb
@@ -6,7 +6,6 @@ require 'pivotal_api'
 class Pick
   
   attr_accessor :api
-  attr_accessor :options
   
   def initialize
     @options = {}
@@ -14,32 +13,49 @@ class Pick
       opts.banner = "Usage: git pick [options]"
       
       opts.on("-k", "--api-key=", "Pivotal Tracker API key") do |k|
-        options[:key] = k
+        @options[:key] = k
       end
       
       opts.on("-p", "--project-id=", "Pivotal Trakcer project id") do |p|
-        options[:project] = p
+        @options[:project] = p
       end
       
       opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-        options[:verbose] = v
+        @options[:verbose] = v
       end
     end.parse!
     
-    @api = PivotalApi.new(options)
+    @api = PivotalApi.new(@options)
   end
   
   def run
-    sys "echo '#{@api.stories.first.name}'"
+    story = @api.stories.first
+    # puts "Story:   #{story.name}"
+    # puts "URL:     #{story.url}"
+    # puts "Branch:  feature-#{story.id}"
+    sys "echo 'Story:  #{story.name}'"
+    sys "echo 'URL:    #{story.url}'"
+    sys "echo 'Branch: feature-#{story.id}'"
   end
   
 private
 
+  def branch_exists?(branch)
+    !get("git branch").match(branch).nil?
+  end
+
   def sys(cmd)
-    puts cmd if options[:verbose]
+    puts cmd if @options[:verbose]
     system cmd
+  end
+  
+  def get(cmd)
+    puts cmd if @options[:verbose]
+    `#{cmd}`
   end
   
 end
 
-Pick.new.run
+if $1 == __FILE__
+  Pick.new.run
+end
