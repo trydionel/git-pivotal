5bfaaccbd44297f6f627a0c5ab5fdf3ebb464275
Jeff Tucker <trydionel@gmail.com>
2010-01-25 18:36:36 -0500

    Update Pick options hash to better match Pivotal::Api parameters

--- lib/commands/pick.rb
+++ lib/commands/pick.rb
@@ -34,15 +34,15 @@ private
     token = get("git config --get pivotal.api-token").strip
     id    = get("git config --get pivotal.project-id").strip
     
-    @options[:key] = token if token
-    @options[:project] = id if id
+    @options[:api_token] = token if token
+    @options[:project_id] = id if id
   end
   
   def parse_argv
     OptionParser.new do |opts|
       opts.banner = "Usage: git pick [options]"
-      opts.on("-k", "--api-key=", "Pivotal Tracker API key") { |k| @options[:key] = k }
-      opts.on("-p", "--project-id=", "Pivotal Trakcer project id") { |p| @options[:project] = p }
+      opts.on("-k", "--api-key=", "Pivotal Tracker API key") { |k| @options[:api_token] = k }
+      opts.on("-p", "--project-id=", "Pivotal Trakcer project id") { |p| @options[:project_id] = p }
       opts.on("-q", "--quiet", "Quiet, no-interaction mode") { |q| @options[:quiet] = q }
       opts.on("-v", "--[no-]verbose", "Run verbosely") { |v| @options[:verbose] = v }
       opts.on_tail("-h", "--help", "This usage guide") { puts opts; exit 0 }
@@ -50,13 +50,13 @@ private
   end
   
   def connect_to_pivotal
-    unless @options[:key] && @options[:project]
+    unless @options[:api_token] && @options[:project_id]
       puts "Pivotal Tracker API Token and Project ID are required"
       exit 0
     end
     
     puts "Retrieving latest stories from Pivotal Tracker..."
-    @api = Pivotal::Api.new(@options)
+    @api = Pivotal::Api.new(:api_token => @options[:api_token])
   end
   
   def agrees?(selection)
@@ -64,8 +64,8 @@ private
   end
   
   def find_story
-    project = @api.projects.find(:id => @options[:project])
-    story = project.stories.first
+    project = @api.projects.find(:id => @options[:project_id])
+    story = project.stories.find(:conditions => { :story_type => :feature, :current_state => :unstarted }, :limit => 1).first
     
     unless story
       puts "No stories available!"
@@ -74,7 +74,7 @@ private
     
     puts "Story: #{story.name}"
     puts "URL:   #{story.url}"
-    print prompt + " (Yn): "
+    print "Accept this story? (Yn): "
     selection = gets.chomp
         
     exit 0 if !agrees?(selection)
@@ -93,11 +93,11 @@ private
     !get("git branch").match(branch).nil?
   end
 
-  def build_feature_branch
+  def build_feature_branch(story)
     branch = "feature-#{story.id}"
 
     puts "Suggested branch name: #{branch}"
-    print prompt + " (Yn): "
+    print "Accept this name? (Yn): "
     selection = gets.chomp
 
     unless agrees?(selection)
