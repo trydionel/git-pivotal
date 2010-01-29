fe845e6f9d0bfc9fd2d156f34b54f3df6df0f707
Jeff Tucker <trydionel@gmail.com>
2010-01-29 18:12:51 -0500

    Improving spec coverage of Pivotal::Story

--- spec/pivotal/story_spec.rb
+++ spec/pivotal/story_spec.rb
@@ -12,4 +12,18 @@ describe Pivotal::Story do
     @story.resource.url.should == "https://www.pivotaltracker.com/services/v3/projects/1/stories/1"
   end
   
+  it "should be able to mark the story as started" do
+    @xml = "<story><current_state>started</current_state></story>"
+    @story.resource.expects(:put).with(@xml)
+
+    @story.start!
+  end
+  
+  it "should be able to update other attributes when marking the story as started" do
+    @xml = "<story><current_state>started</current_state><owned_by>Jeff Tucker</owned_by></story>"
+    @story.resource.expects(:put).with(@xml)
+
+    @story.start!(:owned_by => "Jeff Tucker")
+  end
+  
 end
