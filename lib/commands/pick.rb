752c8250afdcaeeac68407140d62e70135510696
Jeff Tucker <trydionel@gmail.com>
2010-01-29 17:12:27 -0500

    Collect Pivotal Tracker user name and use it to mark ownership when starting a story

--- lib/commands/pick.rb
+++ lib/commands/pick.rb
@@ -33,9 +33,11 @@ private
   def parse_gitconfig
     token = get("git config --get pivotal.api-token").strip
     id    = get("git config --get pivotal.project-id").strip
+    name  = get("git config --get pivotal.full-name").strip
     
     @options[:api_token] = token if token
     @options[:project_id] = id if id
+    @options[:full_name] = name if name
   end
   
   def parse_argv(*args)
@@ -43,6 +45,7 @@ private
       opts.banner = "Usage: git pick [options]"
       opts.on("-k", "--api-key=", "Pivotal Tracker API key") { |k| @options[:api_token] = k }
       opts.on("-p", "--project-id=", "Pivotal Trakcer project id") { |p| @options[:project_id] = p }
+      opts.on("-n", "--full-name=", "Pivotal Trakcer full name") { |n| @options[:full_name] = n }
       opts.on("-q", "--quiet", "Quiet, no-interaction mode") { |q| @options[:quiet] = q }
       opts.on("-v", "--[no-]verbose", "Run verbosely") { |v| @options[:verbose] = v }
       opts.on_tail("-h", "--help", "This usage guide") { puts opts; exit 0 }
@@ -110,7 +113,7 @@ private
   
   def start_story(story)
     puts "Updating story status in Pivotal Tracker..."
-    story.start!
+    story.start!(:owned_by => @options[:full_name])
   end
 
 end
