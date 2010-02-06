e6ae125c3d39bcaf5bbecad3c3a5e41ecc765df3
Jeff Tucker <trydionel@gmail.com>
2010-02-06 13:27:26 -0500

    Close the branch when running git-finish.  Be a bit more verbose in general.

--- lib/commands/finish.rb
+++ lib/commands/finish.rb
@@ -10,16 +10,21 @@ module Commands
       current_branch = get('git status | head -1').gsub(/^.+On branch /, '').chomp
       story_id = current_branch[/\d+/]
       unless story_id
-        puts "Branch name must contain a Pivotal Tracker story id"
+        put "Branch name must contain a Pivotal Tracker story id"
         return 1
       end
 
       api = Pivotal::Api.new(:api_token => options[:api_token])
-
       project = api.projects.find(:id => options[:project_id])
-      story = project.stories.find(story_id)
+      story = project.stories.find(:id => story_id)
       
+      put "Marking Story #{story_id} as finished..."
       story.update_attributes(:current_state => :finished)
+      
+      target_branch = "develop"
+      put "Merging #{current_branch} into #{target_branch}"
+      sys "git checkout #{target_branch}"
+      sys "git merge --no-ff #{current_branch}"
       
       return 0
     end
